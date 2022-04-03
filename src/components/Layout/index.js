891921f9b050e3bf1724890abc3ae104245e26ac
Régis BRUNET BLEC <regis.blec@gmail.com>
2022-04-03 14:48:38 +0200

    mise en place du compo Layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+
+import './index.scss';
+
+import React from 'react'
+
+function Layout() {
+    return (
+        <div>
+            <h1> hello cide</h1>
+        </div>
+    )
+}
+
+export default Layout
